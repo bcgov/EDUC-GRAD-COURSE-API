6416fb389cedd91cf64efeb470e0948da6bc68f6
githubmamatha <harmainimamatha@gmail.com>
2025-05-13 13:03:02 -0700

    Removed unusued imports

--- api/src/test/java/ca/bc/gov/educ/api/course/service/ExaminableCourseServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/course/service/ExaminableCourseServiceTest.java
@@ -2,7 +2,6 @@ package ca.bc.gov.educ.api.course.service;
 
 import ca.bc.gov.educ.api.course.model.dto.Course;
 import ca.bc.gov.educ.api.course.model.dto.ExaminableCourse;
-import ca.bc.gov.educ.api.course.model.dto.ExaminableCourses;
 import ca.bc.gov.educ.api.course.model.entity.ExaminableCourseEntity;
 import ca.bc.gov.educ.api.course.model.transformer.ExaminableCourseTransformer;
 import ca.bc.gov.educ.api.course.repository.ExaminableCourseRepository;
@@ -23,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 
