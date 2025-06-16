1d2100a14f5afe81d12ad0cb2e94b20711503115
QSL\SumathiT <sumathi.thirumani@quartech.com>
2025-06-16 14:32:06 -0700

    Transactional boundary changes applied.

--- api/src/main/java/ca/bc/gov/educ/api/course/service/v2/CourseRestrictionService.java
+++ api/src/main/java/ca/bc/gov/educ/api/course/service/v2/CourseRestrictionService.java
@@ -1,6 +1,5 @@
 package ca.bc.gov.educ.api.course.service.v2;
 
-import ca.bc.gov.educ.api.course.exception.ServiceException;
 import ca.bc.gov.educ.api.course.model.dto.Course;
 import ca.bc.gov.educ.api.course.model.dto.CourseRestrictionRuleData;
 import ca.bc.gov.educ.api.course.model.dto.CourseRestrictionValidationIssue;
@@ -10,11 +9,12 @@ import ca.bc.gov.educ.api.course.model.dto.v2.CourseRestriction;
 import ca.bc.gov.educ.api.course.model.entity.CourseRestrictionsEntity;
 import ca.bc.gov.educ.api.course.repository.CourseRestrictionRepository;
 import ca.bc.gov.educ.api.course.validation.rules.CourseRestrictionRulesProcessor;
-import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
 import java.util.List;
@@ -40,7 +40,7 @@ public class CourseRestrictionService {
     }
 
     public CourseRestriction getCourseRestriction(String mainCourseCode, String mainCourseLevel, String restrictedCourseCode, String restrictedCourseLevel) {
-        Optional<CourseRestrictionsEntity>  courseRestrictionsEntity = courseRestrictionRepository.findByMainCourseAndMainCourseLevelAndRestrictedCourseAndRestrictedCourseLevel(
+        Optional<CourseRestrictionsEntity> courseRestrictionsEntity = courseRestrictionRepository.findByMainCourseAndMainCourseLevelAndRestrictedCourseAndRestrictedCourseLevel(
                 mainCourseCode, mainCourseLevel, restrictedCourseCode, restrictedCourseLevel);
         if (courseRestrictionsEntity.isPresent()) {
             return courseRestrictionMapper.toStructure(courseRestrictionsEntity.get());
@@ -49,60 +49,51 @@ public class CourseRestrictionService {
     }
 
     public CourseRestriction getCourseRestrictionById(UUID courseRestrictionId) {
-        Optional<CourseRestrictionsEntity>  courseRestrictionsEntity = courseRestrictionRepository.findById(courseRestrictionId);
+        Optional<CourseRestrictionsEntity> courseRestrictionsEntity = courseRestrictionRepository.findById(courseRestrictionId);
         if (courseRestrictionsEntity.isPresent()) {
             return courseRestrictionMapper.toStructure(courseRestrictionsEntity.get());
         }
         return null;
     }
 
-    @Retry(name = "generalpostcall")
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public CourseRestrictionValidationIssue updateCourseRestriction(UUID courseRestrictionId, CourseRestriction courseRestriction) {
-        try {
-            CourseRestriction existingCourseRestriction = getCourseRestrictionById(courseRestrictionId);
-            CourseRestrictionRuleData courseRestrictionRuleData = prepareCourseRestrictionRuleData(courseRestriction, true);
-            courseRestrictionRuleData.setExistingCourseRestriction(existingCourseRestriction);
+        CourseRestriction existingCourseRestriction = getCourseRestrictionById(courseRestrictionId);
+        CourseRestrictionRuleData courseRestrictionRuleData = prepareCourseRestrictionRuleData(courseRestriction, true);
+        courseRestrictionRuleData.setExistingCourseRestriction(existingCourseRestriction);
+        List<ValidationIssue> validationIssues = courseRestrictionRulesProcessor.processRules(courseRestrictionRuleData);
+        CourseRestrictionValidationIssue courseRestrictionValidationIssue = createCourseValidationIssue(courseRestriction, validationIssues);
 
-            List<ValidationIssue> validationIssues = courseRestrictionRulesProcessor.processRules(courseRestrictionRuleData);
-            CourseRestrictionValidationIssue courseRestrictionValidationIssue = createCourseValidationIssue(courseRestriction, validationIssues);
-
-            boolean hasError = validationIssues.stream().anyMatch(issue -> "ERROR".equals(issue.getValidationIssueSeverityCode()));
-            if (hasError) {
-                courseRestrictionValidationIssue.setHasPersisted(false);
-            } else {
-                courseRestriction.setCourseRestrictionId(existingCourseRestriction.getCourseRestrictionId());
-                courseRestriction.setCreateDate(existingCourseRestriction.getCreateDate());
-                courseRestriction.setCreateUser(existingCourseRestriction.getCreateUser());
-                persistCourseRestriction(courseRestriction, courseRestrictionValidationIssue);
-            }
-            return courseRestrictionValidationIssue;
-        } catch (Exception e) {
-            throw new ServiceException("Unable to save course restriction.", e);
+        boolean hasError = validationIssues.stream().anyMatch(issue -> "ERROR".equals(issue.getValidationIssueSeverityCode()));
+        if (hasError) {
+            courseRestrictionValidationIssue.setHasPersisted(false);
+        } else {
+            courseRestriction.setCourseRestrictionId(existingCourseRestriction.getCourseRestrictionId());
+            courseRestriction.setCreateDate(existingCourseRestriction.getCreateDate());
+            courseRestriction.setCreateUser(existingCourseRestriction.getCreateUser());
+            persistCourseRestriction(courseRestriction, courseRestrictionValidationIssue);
         }
+        return courseRestrictionValidationIssue;
     }
 
 
-    @Retry(name = "generalpostcall")
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public CourseRestrictionValidationIssue saveCourseRestriction(CourseRestriction courseRestriction) {
-        try {
-            CourseRestriction existingCourseRestriction = this.getCourseRestriction(courseRestriction.getMainCourse(), courseRestriction.getMainCourseLevel(),
-                    courseRestriction.getRestrictedCourse(), courseRestriction.getRestrictedCourseLevel());
-            CourseRestrictionRuleData courseRestrictionRuleData = prepareCourseRestrictionRuleData(courseRestriction, false);
-            courseRestrictionRuleData.setExistingCourseRestriction(existingCourseRestriction);
+        CourseRestriction existingCourseRestriction = this.getCourseRestriction(courseRestriction.getMainCourse(), courseRestriction.getMainCourseLevel(),
+                courseRestriction.getRestrictedCourse(), courseRestriction.getRestrictedCourseLevel());
+        CourseRestrictionRuleData courseRestrictionRuleData = prepareCourseRestrictionRuleData(courseRestriction, false);
+        courseRestrictionRuleData.setExistingCourseRestriction(existingCourseRestriction);
 
-            List<ValidationIssue> validationIssues = courseRestrictionRulesProcessor.processRules(courseRestrictionRuleData);
-            CourseRestrictionValidationIssue courseRestrictionValidationIssue = createCourseValidationIssue(courseRestriction, validationIssues);
+        List<ValidationIssue> validationIssues = courseRestrictionRulesProcessor.processRules(courseRestrictionRuleData);
+        CourseRestrictionValidationIssue courseRestrictionValidationIssue = createCourseValidationIssue(courseRestriction, validationIssues);
 
-            boolean hasError = validationIssues.stream().anyMatch(issue -> "ERROR".equals(issue.getValidationIssueSeverityCode()));
-            if (hasError) {
-                courseRestrictionValidationIssue.setHasPersisted(false);
-            } else {
-                persistCourseRestriction(courseRestriction, courseRestrictionValidationIssue);
-            }
-            return courseRestrictionValidationIssue;
-        } catch (Exception e) {
-            throw new ServiceException("Unable to save course restriction.", e);
+        boolean hasError = validationIssues.stream().anyMatch(issue -> "ERROR".equals(issue.getValidationIssueSeverityCode()));
+        if (hasError) {
+            courseRestrictionValidationIssue.setHasPersisted(false);
+        } else {
+            persistCourseRestriction(courseRestriction, courseRestrictionValidationIssue);
         }
+        return courseRestrictionValidationIssue;
     }
 
     private void persistCourseRestriction(CourseRestriction courseRestriction, CourseRestrictionValidationIssue courseRestrictionValidationIssue) {
@@ -116,7 +107,7 @@ public class CourseRestrictionService {
     private CourseRestrictionRuleData prepareCourseRestrictionRuleData(CourseRestriction courseRestriction, boolean isUpdate) {
         Course mainCourse = courseService.getCourseInfo(courseRestriction.getMainCourse(), courseRestriction.getMainCourseLevel());
         Course restrictedCourse = courseService.getCourseInfo(courseRestriction.getRestrictedCourse(), courseRestriction.getRestrictedCourseLevel());
-        CourseRestrictionRuleData courseRestrictionRuleData= new CourseRestrictionRuleData();
+        CourseRestrictionRuleData courseRestrictionRuleData = new CourseRestrictionRuleData();
         courseRestrictionRuleData.setCourseRestriction(courseRestriction);
         courseRestrictionRuleData.setMainCourse(mainCourse);
         courseRestrictionRuleData.setRestrictedCourse(restrictedCourse);
@@ -124,7 +115,7 @@ public class CourseRestrictionService {
         return courseRestrictionRuleData;
     }
 
-    private CourseRestrictionValidationIssue createCourseValidationIssue(CourseRestriction courseRestriction, List<ValidationIssue> validationIssues){
+    private CourseRestrictionValidationIssue createCourseValidationIssue(CourseRestriction courseRestriction, List<ValidationIssue> validationIssues) {
         CourseRestrictionValidationIssue courseRestrictionValidationIssue = new CourseRestrictionValidationIssue();
         BeanUtils.copyProperties(courseRestriction, courseRestrictionValidationIssue);
         courseRestrictionValidationIssue.setValidationIssues(validationIssues);
