5f2daa76dfa7d28f8e3347eb6ef4c932f36a20a9
arcshiftsolutions <arcshiftsolutions@gmail.com>
2025-11-17 20:04:38 -0800

    Filter criteria

--- api/src/main/java/ca/bc/gov/educ/api/course/model/dto/CourseRegistryEventDTO.java
+++ api/src/main/java/ca/bc/gov/educ/api/course/model/dto/CourseRegistryEventDTO.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.educ.api.course.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CourseRegistryEventDTO {
     private Long id;
     private String affectedTable;
